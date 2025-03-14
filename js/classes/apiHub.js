81e10856e28aa1b612e6b16e6db19cf7c4ec201d
Jonathan <122506441+jwl0724@users.noreply.github.com>
2025-03-14 11:17:26 -0700

    create central api hub for all endpoints

---
+++ js/classes/apiHub.js
@@ -0,0 +1,71 @@
+class APIHub {
+
+    static async login(email, password) {
+        const endpoint = endpoints.backend + routes.login;
+        const response = await fetch(endpoint, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        });
+        return await response.json();
+    }
+
+    static async register(email, password) {
+        const endpoint = endpoints.backend + routes.signup;
+        const response = await fetch(endpoint, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        });
+        return await response.json();
+    }
+
+    static async logout() {
+        const endpoint = endpoints.backend + routes.logout;
+        const response = await fetch(endpoint);
+        if (response.ok) window.location.href = "/";
+    }
+
+    static async chat(text) {
+        const endpoint = endpoints.backend + routes.chat;
+        const response = await fetch(endpoint, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                prompt: text
+            })
+        });
+        return await response.json();
+    }
+
+    static async reset(email) {
+        const endpoint = endpoints.backend + routes.reset;
+        const response = await fetch(endpoint, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email: email
+            })
+        });
+        return await response.json();
+    }
+
+    // TODO: Implement way later
+    static async stt(audio) {
+        const endpoint = endpoints.STT;
+    }
+}
