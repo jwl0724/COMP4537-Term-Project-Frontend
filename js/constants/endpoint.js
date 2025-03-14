c27fcdc41361cdab648170e1baa6265950e6d47f
Jonathan <122506441+jwl0724@users.noreply.github.com>
2025-03-14 11:17:57 -0700

    create container for holding endpoints

---
+++ js/constants/endpoint.js
@@ -0,0 +1,12 @@
+const endpoints = Object.freeze({
+    backend: "http://localhost:8000", // Local testing, need to change when hosted
+    STT: "ep here"
+});
+
+const routes = Object.freeze({
+    login: "/login",
+    signup: "/signup",
+    logout: "/logout",
+    reset: "/reset",
+    chat: "/chat"
+});
