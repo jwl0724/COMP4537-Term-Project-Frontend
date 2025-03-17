8d3372cc30aa9bb0444f9b7380e83033ca019b09
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-03-16 21:20:56 -0700

    add and use util functions

---
+++ js/forgotPassword.js
@@ -0,0 +1,36 @@
+Utils.populateById({
+    title: TITLE,
+    submit: SUBMIT,
+    redirect: LOGIN_REDIRECT,
+    "email-label": PROMPT,
+});
+
+const form = document.getElementById("forgot-password");
+const emailInput = document.getElementById("email");
+const errorMessage = document.getElementById("error-message");
+const submitButton = document.getElementById("submit");
+
+emailInput.addEventListener("input", () => {
+    errorMessage.textContent = "";
+});
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const email = emailInput.value.trim();
+    if (!email) {
+        Utils.populateResponseMessage(errorMessage, ERROR_MISSING_EMAIL);
+        return;
+    }
+    // Input is valid
+    Utils.setButtonState(submitButton, SENDING, true);
+    try {
+        const data = await APIHub.forgotPassword(email);
+        if (!data.error) Utils.populateResponseMessage(errorMessage, ERROR_SERVER, style.redText, style.greenText);
+        else Utils.populateResponseMessage(errorMessage, CONFIRM, style.greenText, style.redText);
+
+    } catch(error) {
+        Utils.populateResponseMessage(errorMessage, ERROR_NETWORK, style.redText, style.greenText)
+    } finally {
+        Utils.setButtonState(submitButton, SUBMIT, false);
+    }
+});
