df59c7604e19bfde9929e1db6531d8c408889250
Juntingg <jsu62@my.bcit.ca>
2025-03-25 20:50:12 -0700

    add admin page user messages

---
+++ lang/en/admin.js
@@ -0,0 +1,8 @@
+const EDIT_BUTTON_TEXT = "Edit";
+const DELETE_BUTTON_TEXT = "Delete";
+const SAVE_BUTTON_TEXT = "Save";
+const CONTROL_BUTTONS_COL_TEXT = "Actions";
+const SUCCESS_UPDATE_WINDOW = "User updated!";
+const FAILED_UPDATE_WINDOW = "Failed to update user!";
+const SUCCESS_DELETE_WINDOW = "User deleted!";
+const FAILED_DELETE_WINDOW = "Failed to delete user!";
