727d4ada748f416112835e69ab4b427a09c6f421
Juntingg <jsu62@my.bcit.ca>
2025-03-25 21:08:57 -0700

    separate userHandler from handleTable class

--- js/classes/handleTable.js
+++ js/classes/handleTable.js
@@ -22,12 +22,12 @@ class HandleTable {
             for (const header of headerList) {
                 insertContent += `<td class='px-6 py-4 ${header}'>${row[header]}</td>`;
             }
-            if (add_edit) {
+            if (add_edit && row["role"] !== "admin") {
                 console.log(row["email"]);
                 insertContent += `<td class='px-6 py-4 flex justify-between' id='${row[1]}'>
-                <button class='text-blue-700 edit-button' onClick="HandleTable.editUser('${row["email"]}')">${EDIT_BUTTON_TEXT}</button>
-                <button class='text-blue-700 save-button hidden' onClick="HandleTable.saveUser('${row["email"]}')">${SAVE_BUTTON_TEXT}</button>
-                <button class='text-red-500' onClick="HandleTable.deleteUser('${row["email"]}')">${DELETE_BUTTON_TEXT}</button>
+                <button class='text-blue-700 edit-button' onClick="UserHandler.editUser('${row["email"]}')">${EDIT_BUTTON_TEXT}</button>
+                <button class='text-blue-700 save-button hidden' onClick="UserHandler.saveUser('${row["email"]}')">${SAVE_BUTTON_TEXT}</button>
+                <button class='text-red-500' onClick="UserHandler.deleteUser('${row["email"]}')">${DELETE_BUTTON_TEXT}</button>
                 </td>`;
             }
             insertContent += "</tr>";
@@ -36,64 +36,7 @@ class HandleTable {
         table.innerHTML = insertContent;
     }
 
-    static async deleteUser(email) {
-        const rowElement = document.getElementById(email);
-        if (rowElement) {
-            try{
-                const response = await APIHub.deleteUser(email);
-                if(response){
-                    rowElement.remove();
-                    alert(SUCCESS_DELETE_WINDOW);
-                }else{
-                    alert(FAILED_DELETE_WINDOW);
-                }
-            } catch (error) {
-                alert(FAILED_DELETE_WINDOW);
-            }
-
-
-        }
-    }
-
-    static async editUser(email) {
-        const rowElement = document.getElementById(email);
-        const apiCell = rowElement.querySelector('.api_calls_left'); 
-
-        const apiValue = apiCell.textContent;
-        apiCell.innerHTML = `<input type="number" value="${apiValue}" class="api-input w-20 px-2 py-1 border border-gray-300 rounded"/>`;
-
-        const editButton = rowElement.querySelector(`.edit-button`);
-        const saveButton = rowElement.querySelector(`.save-button`);
-        editButton.classList.add('hidden');
-        saveButton.classList.remove('hidden');
-    }
-
-    static async saveUser(email) {
-        const rowElement = document.getElementById(email);
-        const apiInput = rowElement.querySelector('.api-input');  
-        const newApiValue = apiInput.value;  
-
-        try{
-            const response = await APIHub.updateApiCalls(email, { api_calls_left: newApiValue });
-            if(response){
-                        const apiCell = rowElement.querySelector('.api_calls_left');
-                        apiCell.innerHTML = newApiValue; 
-                
-                        const editButton = rowElement.querySelector(`.edit-button`);
-                        const saveButton = rowElement.querySelector(`.save-button`);
-                        editButton.classList.remove('hidden');
-                        saveButton.classList.add('hidden');
-                        alert(SUCCESS_UPDATE_WINDOW);
-            }else{
-                alert(FAILED_UPDATE_WINDOW);
-            }
-        }catch(error){
-            alert(FAILED_UPDATE_WINDOW);
-        }
-
-        
-
-    }
+    
 
     static async renderUserTable() {
         const data = await APIHub.getAllUsers();
