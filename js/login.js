be84d8d29e66176b643bba5952b92cf218824ecd
Juntingg <jsu62@my.bcit.ca>
2025-03-30 16:13:00 -0700

    fix id name

--- js/login.js
+++ js/login.js
@@ -2,8 +2,8 @@ Utils.populateById({
     title: TITLE,
     'email-label': EMAIL_LABEL,
     'password-label': PASSWORD_LABEL,
-    'forgot-password': FORGOT_PASSWORD,
-    submit: SUBMIT
+    submit: SUBMIT,
+    'forgot-password': FORGOT_PASSWORD
 });
 
 Utils.populatePlaceholderById({
