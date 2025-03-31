42a8d58fdf312460301d704521fd042e7aacc4f5
Juntingg <jsu62@my.bcit.ca>
2025-03-30 17:02:26 -0700

    add comments

--- js/classes/presentation/sprite.js
+++ js/classes/presentation/sprite.js
@@ -20,7 +20,7 @@ class Sprite {
         neutral: "neutral"
     });
 
-    #imageField;
+    #imageField;// Stores the current emotion
 
     constructor() {
         this.#imageField = document.getElementById("spongebob");
@@ -29,22 +29,26 @@ class Sprite {
         this.#playAnimation(Sprite.#animationNames.FADE_IN, 0.85);
     }
 
+    // Toggle between thinking and normal state
     toggleThink(isThinking) {
         this.#imageField.src = images[Sprite.emotions.think];
         if (isThinking) this.#playAnimation(Sprite.#animationNames.THINK, 0.2);
         else this.#playAnimation(Sprite.#animationNames.UNTHINK, 0.2);
     }
 
+    // Change the sprite's emotion
     emote(emotion) {
         this.#imageField.src = images[emotion];
         this.#playAnimation(Sprite.#animationNames.NEXT_EMOTION, 0.15);
     }
 
+    // Set up animation properties for smoother transitions
     #setupAnimationProperties() {
         this.#imageField.style.animationIterationCount = 1;
         this.#imageField.style.transitionTimingFunction = "ease-in-out";
     }
 
+    // Play a specified animation with a given duration
     #playAnimation(animationName, durationSeconds) {
         this.#imageField.style.animation = "none";
         this.#imageField.offsetHeight; // Forces reflow (i.e. lets animation play again)
